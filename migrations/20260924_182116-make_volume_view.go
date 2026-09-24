47c1b2311a86f2724a676f4b2ecae561599709cb
Adam Bibby <adam@bibby.io>
2026-09-24 18:41:24 -0400

    add series view

---
+++ migrations/20260924_182116-make_volume_view.go
@@ -0,0 +1,55 @@
+package migrations
+
+import (
+	"context"
+
+	"gosalusa.com/database"
+	"gosalusa.com/database/migrate"
+	"gosalusa.com/database/schema"
+)
+
+func init() {
+	migrations.Add(&migrate.Migration{
+		Name: "20260924_182116-make_volume_view",
+		Up: schema.Raw(`
+CREATE MATERIALIZED VIEW volumes as
+WITH stacked_sources AS (
+	SELECT int_viz_volumes.series_id AS source_series_id,
+		'viz'::text AS source,
+		int_viz_volumes.number::text AS number,
+		50 AS quality,
+		int_viz_volumes.cover_url::text AS cover_url
+	FROM int_viz_volumes
+	UNION ALL
+	SELECT int_mangadex_volumes.series_id AS source_series_id,
+		'mangadex'::text AS source,
+		int_mangadex_volumes.number,
+		1 AS quality,
+		int_mangadex_volumes.cover_url
+	FROM int_mangadex_volumes
+),
+stacked_sources_with_id AS (
+	SELECT id_maps.series_id,
+		stacked_sources.source_series_id,
+		stacked_sources.source,
+		stacked_sources.number,
+		stacked_sources.quality,
+		stacked_sources.cover_url
+	FROM stacked_sources
+	JOIN id_maps ON stacked_sources.source_series_id = id_maps.source_series_id AND stacked_sources.source = id_maps.source
+	WHERE stacked_sources.number IS NOT NULL
+)
+SELECT
+	series_id,
+	number,
+	(array_remove(array_agg(cover_url ORDER BY quality DESC), NULL::text))[1] AS cover_url
+FROM stacked_sources_with_id
+GROUP BY series_id, number;
+
+CREATE UNIQUE INDEX volumes_series_number_idx ON volumes (series_id, number);
+`),
+		Down: schema.Run(func(ctx context.Context, tx database.DB) error {
+			return nil
+		}),
+	})
+}
